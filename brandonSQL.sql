9cbef3c0a21a36eb903a06a94c6589bc94ec0e97
Brandon Sellers <bms2993@users.noreply.github.com>
2018-11-08 20:07:09 -0600

    Added 1st answers for chapters 1 and 2

--- brandonSQL.sql
+++ brandonSQL.sql
@@ -1,5 +1,53 @@
-/* Brandon has answer table 1 and 4*/
+/* Brandon has answer table 1 and 2*/
 /* Correct is either a y or an n */
 
+/* Chapter 1 */
+
 INSERT INTO Answer1 (questionID, answer, correct)
-VALUES ('1', 'whatever', '');
+VALUES ('1', 'Why does computer hardware cost so much?', 'y');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('2', 'True', 'n');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('3', 'Software suffers from exposure to hostile environments', 'n');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('4', 'True', 'n');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('5', 'True', 'n');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('6', 'True', 'y');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('7', 'True', 'y');
+
+/* Chapter 2 */
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('8', 'Process', 'n');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('9', 'True', 'n');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('10', 'communication, planning, modeling, construction, deployment', 'y');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('11', 'True', 'y');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('12', 'True', 'y');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('13', 'True', 'n');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('14', 'True', 'y');
+
+INSERT INTO Answer1 (questionID, answer, correct)
+VALUES ('15', 'True', 'n');
+
+/* Chapter 3 */
