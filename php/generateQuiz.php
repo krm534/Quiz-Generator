11e3623be0460a77c2ef1f431cee3db5040fe703
Logan Cummins <logan8484@gmail.com>
2018-11-24 14:37:21 -0600

    generate quiz and post to results
    generate quiz can now intake a string of questionIDs from savedquizzes, and then the startings of results page as well

--- php/generateQuiz.php
+++ php/generateQuiz.php
@@ -14,10 +14,28 @@ function fixString($string) {
     return preg_replace('/[^\x00-\x7f]/', '\'', $string);
 }
 
+//function that prints out the the quiz questions
+function printQuestion($row){
+    //for each answer, the pair of [answer, questionID] is passed into the post array
+    echo fixString($row["question"]) . "<br>";
+    echo '<input type="radio" name="question'. $row['quesID']. '" value="[a, '.$row['quesID'].']">A = ' . fixString($row["A"]) . '<br>';
+    echo '<input type="radio" name="question'. $row['quesID']. '" value="[b, '.$row['quesID'].']">B = ' . fixString($row["B"]) . '<br>';
+    if($row["C"] != null){
+        echo '<input type="radio" name="question'. $row['quesID']. '" value="[c, '.$row['quesID'].']">C = ' . fixString($row["C"]) . '<br>';
+    }
+    if($row["D"] != null){
+        echo '<input type="radio" name="question'. $row['quesID']. '" value="[d, '.$row['quesID'].']">D = ' . fixString($row["D"]) . '<br>';
+    }
+    if($row["E"] != null){
+        echo '<input type="radio" name="question'. $row['quesID']. '" value="[e, '.$row['quesID'].']">E = ' . fixString($row["E"]) . '<br>';
+    }
+    echo '<br>';    
+}
+
 $servername = "localhost";
 $username = "root";
 //your password
-$password = "Hellias8484";
+$password = "";
 $database = "seQuiz";
 
 // Create connection
@@ -31,13 +49,18 @@ echo "Connected successfully<br>";
 */
 
 //each index corresponds with a chapter and each value corresponds with the number of questions
-$questions = array(2,1,2,0,0,5,0,1,0,10);
+//CHANGE THIS TO GET FROM THE POST ARRAY TO CONNECT TO HOMEPAGE
+$questions = array(4,0,0,2,0,0,0,0,0,0);
 
-/*
-1) get the array of different amounts of questions
-2) for each chapter wanted get that many questions
-3) print it out in the form of question and X answers
-*/
+//this would handle for the saved quizzes passing in the text of 
+//CHANGE THIS TO GET FROM THE POST ARRAY TO CONNECT TO THE SAVEDQUIZZES PAGE
+$savedQuestionsText = '';
+if(($savedQuestionsText == null or $savedQuestionsText == '')){
+    $savedQuestions = null; 
+}
+else{
+    $savedQuestions = explode(', ', $savedQuestionsText);
+}
 
 $chapterQuestionPair = array(); 
 //create pairs of chapters and amount of questions
@@ -46,6 +69,7 @@ for($it = 0; $it < 10; $it++){
         array_push($chapterQuestionPair, array(($it+1), $questions[$it]));
 	}
 }
+
 /*
 debugging the chapter-question pairs
 for($i = 0; $i < count($chapterQuestionPair); $i++){
@@ -58,42 +82,70 @@ for($i = 0; $i < count($chapterQuestionPair); $i++){
 */
 //selects every question and answer
 //select question, Answer1.answer as A,Answer2.answer as B,Answer3.answer as C, Answer4.answer as D, Answer5.answer as E from Questions, Answer1, Answer2,Answer3,Answer4,Answer5 where Questions.questionID = Answer1.questionID and Questions.questionID = Answer2.questionID and Questions.questionID = Answer3.questionID and Questions.questionID = Answer4.questionID and Questions.questionID = Answer5.questionID;
-echo '<form>';
-//Make each query and send it off to find the results
-for($i = 0; $i < count($chapterQuestionPair); $i++){
-    $questionAnswerQuery = "select chapter, question, Answer1.answer as A,Answer2.answer as B,Answer3.answer as C, Answer4.answer as D, Answer5.answer as E from Questions, Answer1, Answer2,Answer3,Answer4,Answer5 where Questions.questionID = Answer1.questionID and Questions.questionID = Answer2.questionID and Questions.questionID = Answer3.questionID and Questions.questionID = Answer4.questionID and Questions.questionID = Answer5.questionID";
-    $chapter = $chapterQuestionPair[$i][0];
-    $numberOfQuestions = $chapterQuestionPair[$i][1];
-    $questionCounter = 0; 
-    $questionAnswerQuery = $questionAnswerQuery . " and Questions.chapter = " . $chapter;
-    $results = $conn->query($questionAnswerQuery);
-    
-    if($results->num_rows > 0){
-        //output the data found
-        while($row = $results->fetch_assoc()){
-            //compare the questionCounter to the numberOfQuestions to only print out the amount of questions the array calls for
-            if($questionCounter < $numberOfQuestions){
-                echo fixString($row["question"]) . "<br>";
-                echo '<input type="radio" name="chapter'. $row["chapter"]. 'question' . ($questionCounter+1) . '" value="a">A = ' . fixString($row["A"]) . '<br>';
-                echo '<input type="radio" name="chapter'. $row["chapter"]. 'question' . ($questionCounter+1) . '" value="b">B = ' . fixString($row["B"]) . '<br>';
-                if($row["C"] != null){
-                    echo '<input type="radio" name="chapter'. $row["chapter"]. 'question' . ($questionCounter+1) . '" value="c">C = ' . fixString($row["C"]) . '<br>';
-                }
-                if($row["D"] != null){
-                    echo '<input type="radio" name="chapter'. $row["chapter"]. 'question' . ($questionCounter+1) . '" value="d">D = ' . fixString($row["D"]) . '<br>';
-                }
-                if($row["E"] != null){
-                    echo '<input type="radio" name="chapter'. $row["chapter"]. 'question' . ($questionCounter+1) . '" value="e">E = ' . fixString($row["E"]) . '<br>';
-                }
-                echo '<br>';
-            }
-            $questionCounter = $questionCounter + 1;
+echo '<form action="results.php" method="post">';
+if($savedQuestions){
+    $questionAnswerQuery = "select Questions.questionID as quesID, chapter, question, Answer1.answer as A,Answer2.answer as B,Answer3.answer as C, Answer4.answer as D, Answer5.answer as E from Questions, Answer1, Answer2,Answer3,Answer4,Answer5 where Questions.questionID = Answer1.questionID and Questions.questionID = Answer2.questionID and Questions.questionID = Answer3.questionID and Questions.questionID = Answer4.questionID and Questions.questionID = Answer5.questionID and (";
+    //loop through the array adding the questionIDs to the query
+    for($x = 0; $x < count($savedQuestions); $x++){
+        //if it is the last element of the array end the query
+        if($x == (count($savedQuestions)) - 1){
+            $questionAnswerQuery = $questionAnswerQuery . " Questions.questionID = " . $savedQuestions[$x] . ")"; 
+        }
+        else{
+            $questionAnswerQuery = $questionAnswerQuery . " Questions.questionID = " . $savedQuestions[$x] . " or";
         }
     }
-    else{
-        echo 'Result is 0';
+    $results = $conn->query($questionAnswerQuery);
+    if($results->num_rows > 0){
+        while($row = $results->fetch_assoc()){
+            printQuestion($row);
+        }
     }
 }
+else{
+    $questionIdString = '';
+    //Make each query and send it off to find the results
+    for($i = 0; $i < count($chapterQuestionPair); $i++){
+        $questionAnswerQuery = "select Questions.questionID as quesID, chapter, question, Answer1.answer as A,Answer2.answer as B,Answer3.answer as C, Answer4.answer as D, Answer5.answer as E from Questions, Answer1, Answer2,Answer3,Answer4,Answer5 where Questions.questionID = Answer1.questionID and Questions.questionID = Answer2.questionID and Questions.questionID = Answer3.questionID and Questions.questionID = Answer4.questionID and Questions.questionID = Answer5.questionID";
+        $chapter = $chapterQuestionPair[$i][0];
+        $numberOfQuestions = $chapterQuestionPair[$i][1]; 
+        $questionAnswerQuery = $questionAnswerQuery . " and Questions.chapter = " . $chapter;
+        $results = $conn->query($questionAnswerQuery);
+
+        //prepare the results of the query to be printed out
+        if($results->num_rows > 0){
+            $randomArray = array(); 
+            //output the data found
+            while($row = $results->fetch_assoc()){
+                array_push($randomArray, $row);
+            }
+            //randomize the array of questions from the chapter
+            shuffle($randomArray);
+            //print out the number of quesitons
+            for($x = 0; $x < $numberOfQuestions; $x++){
+                printQuestion($randomArray[$x]);
+                //make the string to pass to the saved querry table
+                $questionIdString = $questionIdString . $randomArray[$x]['quesID'] . ', ';
+            }
+        }
+        else{
+            echo 'Result is 0';
+        }
+    }
+    //remove the last ', '
+    $questionIdString = substr($questionIdString, 0, -2);
+    //makes the chapter array into a string
+    $chapterString = implode(',', $questions); 
+    //checks if the exact quiz already exists in the saved quizzes database
+    $checkSavedQuizzes = "select * from SavedQuizzes where questions = '" . $questionIdString . "' and chapters = '" . $chapterString . "'";
+    $checkSaved = $conn->query($checkSavedQuizzes);
+    //if it does not exist, put the quiz in the database
+    if($checkSaved->num_row == 0){
+        $savedQuizzesQuery = "insert into SavedQuizzes(questions, chapters) values ('". $questionIdString ."', '" . $chapterString . "')";
+        $results = $conn->query($savedQuizzesQuery);
+    }
+}
+echo '<button type="submit" name="submit">Submit</button>';
 echo '</form>';
 ?>
   
---
+++ php/results.php
@@ -0,0 +1,7 @@
+<?php
+echo 'show how long the post array is using count<br>';
+echo count($_POST) . "<br>";
+echo 'show the contents of post array using print_r<br>';
+print_r($_POST);
+
+?>
